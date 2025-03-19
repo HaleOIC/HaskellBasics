4a1330f17c0aa32d62517c854d1ff10249661dee
HaleOIC <shinehale730@gmail.com>
2025-03-19 18:16:13 +0100

    finish Set16

--- exercises/Set16a.hs
+++ exercises/Set16a.hs
@@ -17,7 +17,11 @@ import           Data.List
 --  *Set16a> quickCheck (isSorted [])
 --  +++ OK, passed 1 test.
 isSorted :: (Show a, Ord a) => [a] -> Property
-isSorted = todo
+isSorted xs =
+  case length xs of
+    0 -> property True
+    1 -> property True
+    _ -> property $ and [xs !! i <= xs !! (i + 1) | i <- [0 .. (length xs - 2)]]
 
 ------------------------------------------------------------------------------
 -- Ex 2: In this and the following exercises, we'll build a suite of
@@ -48,7 +52,10 @@ isSorted = todo
 --  *Set16a> quickCheck (sumIsLength [4,5,6,4,5,4] (freq1 [4,5,6,4,5,4]))
 --  +++ OK, passed 1 test.
 sumIsLength :: Show a => [a] -> [(a, Int)] -> Property
-sumIsLength input output = todo
+sumIsLength input output = (length input) === (sumOuput output)
+  where
+    sumOuput ((c, freq):xs) = freq + sumOuput xs
+    sumOuput []             = 0
 
 -- This is a function that passes the sumIsLength test but is wrong
 freq1 :: Eq a => [a] -> [(a, Int)]
@@ -76,7 +83,8 @@ freq1 (x:y:xs) = [(x, 1), (y, length xs + 1)]
 --  *Set16a> quickCheck (inputInOutput [4,5,6,4,5,4] (freq2 [4,5,6,4,5,4]))
 --  +++ OK, passed 100 tests.
 inputInOutput :: (Show a, Eq a) => [a] -> [(a, Int)] -> Property
-inputInOutput input output = todo
+inputInOutput input output =
+  forAll (elements input) (\x -> any (\(y, _) -> x == y) output)
 
 -- This function passes both the sumIsLength and inputInOutput tests
 freq2 :: Eq a => [a] -> [(a, Int)]
@@ -106,7 +114,10 @@ freq2 xs = map (\x -> (x, 1)) xs
 --  *Set16a> quickCheck (outputInInput [4,5,6,4,5,4] (freq3 [4,5,6,4,5,4]))
 --  +++ OK, passed 100 tests.
 outputInInput :: (Show a, Eq a) => [a] -> [(a, Int)] -> Property
-outputInInput input output = todo
+outputInInput input output =
+  forAll
+    (elements output)
+    (\(x, count) -> (length $ filter (== x) input) === count)
 
 -- This function passes the outputInInput test but not the others
 freq3 :: Eq a => [a] -> [(a, Int)]
@@ -134,7 +145,10 @@ freq3 (x:xs) = [(x, 1 + length (filter (== x) xs))]
 --  *Set16a> quickCheck (frequenciesProp frequencies)
 --  +++ OK, passed 100 tests.
 frequenciesProp :: ([Char] -> [(Char, Int)]) -> NonEmptyList Char -> Property
-frequenciesProp freq input = todo
+frequenciesProp freq (NonEmpty input) =
+  let output = freq input
+   in sumIsLength input output .&&. inputInOutput input output .&&.
+      outputInInput input output
 
 frequencies :: Eq a => [a] -> [(a, Int)]
 frequencies [] = []
@@ -165,7 +179,10 @@ frequencies (x:ys) = (x, length xs) : frequencies others
 --  [0,1,8]
 --  [2,4,10]
 genList :: Gen [Int]
-genList = todo
+genList = do
+  n <- choose (3, 5)
+  unsorted <- vectorOf n (choose (0, 10))
+  return (sort unsorted)
 
 ------------------------------------------------------------------------------
 -- Ex 7: Here are the datatypes Arg and Expression from Set 15. Write
@@ -206,7 +223,12 @@ data Expression
   deriving (Show, Eq)
 
 instance Arbitrary Arg where
-  arbitrary = todo
+  arbitrary =
+    oneof
+      [ Number <$> choose (0, 10)
+      , Variable <$> elements ['a', 'b', 'c', 'x', 'y', 'z']
+      ]
 
 instance Arbitrary Expression where
-  arbitrary = todo
+  arbitrary =
+    oneof [Plus <$> arbitrary <*> arbitrary, Minus <$> arbitrary <*> arbitrary]
--- exercises/Set16b.hs
+++ exercises/Set16b.hs
@@ -1,16 +1,18 @@
 module Set16b where
 
-import Mooc.Todo
-import Examples.Phantom
+import           Examples.Phantom
+import           Mooc.Todo
 
-import Data.Char (toUpper)
+import           Data.Char        (toUpper)
 
 ------------------------------------------------------------------------------
 -- Ex 1: Define a constant pounds with type Money GBP and a value of
 -- 3. The type Money is imported from Example.Phantom but you'll need
 -- to introduce GBP yourself.
+data GBP
 
-pounds = todo
+pounds :: Money GBP
+pounds = Money 3
 
 ------------------------------------------------------------------------------
 -- Ex 2: Implement composition for Rates. Give composeRates a
@@ -22,12 +24,12 @@ pounds = todo
 --   composeRates eurToUsd (invert eurToUsd) :: Rate EUR EUR
 --   composeRates eurToUsd eurToUsd :: type error!
 --   composeRates eurToUsd :: Rate USD to -> Rate EUR to
-
 -- For testing
 usdToChf :: Rate USD CHF
 usdToChf = Rate 1.11
 
-composeRates rate1 rate2 = todo
+composeRates :: Rate from mid -> Rate mid to -> Rate from to
+composeRates (Rate r1) (Rate r2) = Rate $ r1 * r2
 
 ------------------------------------------------------------------------------
 -- Ex 3: Tracking first, last and full names with phantom types. The
@@ -46,19 +48,26 @@ composeRates rate1 rate2 = todo
 --  fromName (toLast "smith") ==> "smith"
 --  toFirst "bob" :: Name First
 --  toLast "smith" :: Name Last
+data First
 
+data Last
+
+data Full
+
+data Name a =
+  Name String
 
 -- Get the String contained in a name
---fromName :: Name a -> String
-fromName = todo
+fromName :: Name a -> String
+fromName (Name str) = str
 
 -- Build a Name First
---toFirst :: String -> Name First
-toFirst = todo
+toFirst :: String -> Name First
+toFirst str = Name str
 
 -- Build a Name Last
---toLast :: String -> Name Last
-toLast = todo
+toLast :: String -> Name Last
+toLast str = Name str
 
 ------------------------------------------------------------------------------
 -- Ex 4: Implement the functions capitalize and toFull.
@@ -77,10 +86,12 @@ toLast = todo
 --  fromName (capitalize (toFirst "bob")) ==> "Bob"
 --  capitalize (toLast "smith") :: Name Last
 --  fromName (capitalize (toLast "smith")) ==> "Smith"
+capitalize :: Name a -> Name a
+capitalize (Name "")     = Name ""
+capitalize (Name (c:cs)) = Name (toUpper c : cs)
 
-capitalize = todo
-
-toFull = todo
+toFull :: Name First -> Name Last -> Name Full
+toFull (Name first) (Name last) = Name (first ++ " " ++ last)
 
 ------------------------------------------------------------------------------
 -- Ex 5: Type classes can let you write code that handles different
@@ -90,7 +101,14 @@ toFull = todo
 --  render (Money 1.0 :: Money EUR) ==> "1.0e"
 --  render (Money 1.0 :: Money USD) ==> "$1.0"
 --  render (Money 1.0 :: Money CHF) ==> "1.0chf"
-
 class Render currency where
   render :: Money currency -> String
 
+instance Render EUR where
+  render (Money m) = show m ++ "e"
+
+instance Render USD where
+  render (Money m) = "$" ++ show m
+
+instance Render CHF where
+  render (Money m) = show m ++ "chf"
